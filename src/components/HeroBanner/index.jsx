d3fd87d721da216a23f919cd1afb7ccd2709341d
Habib Ur Rehman <habib.urrehman.nawaz@gmail.com>
2023-08-19 18:41:24 +0500

    Added carousel to view movies list in hero and layout

--- src/components/HeroBanner/index.jsx
+++ src/components/HeroBanner/index.jsx
@@ -1,18 +1,34 @@
 import styles from "./heroBanner.module.css";
 import data from "../../data/homepage.json";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function HeroBanner() {
   const heroTitles = data.titles.filter((title) => title.moduleType === "HERO");
+
+  const settings = {
+    slidesToShow: 2.3,
+    infinite: true,
+    autoplay: true,
+    speed: 800,
+    arrows: false,
+    dots: false,
+    centerMode: true,
+  };
+
   return (
     <div className={styles.container}>
-      {heroTitles.flatMap((heroTitle) =>
-        heroTitle.layoutTitles.titles.map((title) => (
-          <img
-            src={title.thumbnails["thumb-613x1536"].url}
-            alt={title.thumbnails["thumb-613x1536"].title}
-          ></img>
-        ))
-      )}
+      <Slider {...settings}>
+        {heroTitles.flatMap((heroTitle) =>
+          heroTitle.layoutTitles.titles.map((title) => (
+            <img
+              src={title.thumbnails["thumb-613x1536"].url}
+              alt={title.thumbnails["thumb-613x1536"].title}
+            ></img>
+          ))
+        )}
+      </Slider>
     </div>
   );
 }
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,17 +1,34 @@
 import styles from "./layout.module.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./custom.css";
 
 export default function Layout(props) {
   const { heading, titles } = props;
+  const settings = {
+    slidesToShow: 19,
+    slidesToScroll: 5,
+    swipeToSlide: true,
+    infinite: titles.length > 19,
+    autoplay: false,
+    arrows: false,
+    dots: false,
+    centerMode: true,
+    adaptiveHeight: true,
+  };
   return (
     <div className={styles.container}>
       <div className={styles.heading}>{heading}</div>
       <div className={styles.content}>
-        {titles.map((title) => (
-          <img
-            src={title.thumbnails["thumb-677x474"].url}
-            alt={title.thumbnails["thumb-677x474"].title}
-          ></img>
-        ))}
+        <Slider {...settings}>
+          {titles.map((title) => (
+            <img
+              src={title.thumbnails["thumb-677x474"].url}
+              alt={title.thumbnails["thumb-677x474"].title}
+            ></img>
+          ))}
+        </Slider>
       </div>
     </div>
   );
